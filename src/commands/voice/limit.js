0acbb14749de9799fbae56b639e88c7cd1ebe307
Frain. <119671451+xDevFrain@users.noreply.github.com>
2024-09-06 02:31:50 +0300

    Solve Errors.

--- src/commands/voice/limit.js
+++ src/commands/voice/limit.js
@@ -4,13 +4,16 @@ module.exports = async (client, interaction, args) => {
     const perms = await client.checkBotPerms({
         flags: [Discord.PermissionsBitField.Flags.ManageChannels],
         perms: [Discord.PermissionsBitField.Flags.ManageChannels]
-    }, interaction)
+    }, interaction);
 
-    if (!perms) return;
+    if (!perms) {
+        console.log('Bot does not have sufficient permissions to manage channels.');
+        return;
+    }
 
     let limit = interaction.options.getNumber('limit');
-
     const channel = interaction.member.voice.channel;
+
     if (!channel) {
         return client.errNormal({
             error: `You're not in a voice channel!`,
@@ -19,14 +22,13 @@ module.exports = async (client, interaction, args) => {
     }
 
     var checkVoice = await client.checkVoice(interaction.guild, channel);
-    
     if (!checkVoice) {
         console.log(`Failed checkVoice for channel: ${channel.name} (ID: ${channel.id})`);
         return client.errNormal({
             error: `You cannot edit this channel!`,
             type: 'editreply'
         }, interaction);
-    } 
+    }
 
     try {
         await channel.setUserLimit(limit);
