3e94952e91d422636c5dc614422834568afa63e7
Frain. <119671451+xDevFrain@users.noreply.github.com>
2024-09-05 21:36:34 +0300

    Solve Errors.

--- src/commands/tools/remind.js
+++ src/commands/tools/remind.js
@@ -7,29 +7,49 @@ module.exports = async (client, interaction, args) => {
     const text = interaction.options.getString('message');
 
     const timeInMs = ms(time);
-    if (!timeInMs) {
-        return client.errNormal({ error: "Invalid time format!", type: 'editreply' }, interaction);
+    if (!timeInMs || timeInMs <= 0) {
+        return client.errNormal({ 
+            error: "Invalid time format! Please use formats like '10m', '1h', etc.", 
+            type: 'editreply' 
+        }, interaction);
     }
 
-    const endtime = new Date().getTime() + timeInMs;
+    const endtime = Date.now() + timeInMs;
 
-    Schema.findOne({ Text: text, User: interaction.user.id, endTime: endtime }, async (err, data) => {
-        if (data) {
-            return client.errNormal({ error: `You already made this reminder!`, type: 'editreply' }, interaction);
+    Schema.findOne({ Text: text, User: interaction.user.id }, async (err, data) => {
+        if (err) {
+            console.error(err);
+            return client.errNormal({ 
+                error: "An error occurred while accessing the database.", 
+                type: 'editreply' 
+            }, interaction);
         }
-        else {
+
+        if (data) {
+            return client.errNormal({ 
+                error: `You already made this reminder!`, 
+                type: 'editreply' 
+            }, interaction);
+        } else {
+            await new Schema({
+                Text: text,
+                User: interaction.user.id,
+                endTime: endtime
+            }).save();
+
             return client.succNormal({
                 text: `Your reminder is set!`,
-                fields: [{
-                    name: `${client.emotes.normal.clock}┇End Time`,
-                    value: `${new Date(endtime).toLocaleString()}`, // استخدام toLocaleString للحصول على التاريخ والوقت
-                    inline: true,
-                },
-                {
-                    name: `💭┇Reminder`,
-                    value: `${text}`,
-                    inline: true,
-                }
+                fields: [
+                    {
+                        name: `${client.emotes.normal.clock}┇End Time`,
+                        value: `${new Date(endtime).toLocaleString()}`,
+                        inline: true,
+                    },
+                    {
+                        name: `💭┇Reminder`,
+                        value: `${text}`,
+                        inline: true,
+                    }
                 ],
                 type: 'editreply'
             }, interaction);
@@ -49,6 +69,6 @@ module.exports = async (client, interaction, args) => {
             ],
         }, interaction.user);
 
-        await Schema.findOneAndDelete({ Text: text, User: interaction.user.id, endTime: endtime });
-    }, endtime - new Date().getTime());
+        await Schema.findOneAndDelete({ Text: text, User: interaction.user.id });
+    }, timeInMs);
 };
